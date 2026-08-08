a0bce6455ac67bf6c7b2cfa0aeee623e45f06214
Alberto Rivera <albertorivera@Mac.localdomain>
2026-08-07 20:25:31 -0400

    feat: improve local business structured data

--- components/seo/StructuredData.tsx
+++ components/seo/StructuredData.tsx
@@ -1,6 +1,7 @@
 export default function StructuredData() {
   const data = {
     "@context": "https://schema.org",
+
     "@type": "ProfessionalService",
 
     name:
@@ -9,14 +10,22 @@ export default function StructuredData() {
     url:
       "https://www.cbeipr.com",
 
+    image:
+      "https://www.cbeipr.com/logo/logo-3-final.webp",
+
     description:
       "Servicios de psicología clínica, terapia emocional, evaluaciones psicológicas y acompañamiento profesional.",
 
-    areaServed:
-      "Puerto Rico",
+
+    areaServed: {
+      "@type": "AdministrativeArea",
+      name: "Puerto Rico",
+    },
+
 
     serviceType:
       "Psicología Clínica",
+
 
     founder: {
       "@type": "Person",
@@ -27,7 +36,85 @@ export default function StructuredData() {
       jobTitle:
         "Psicóloga Clínica",
     },
+
+
+    address: {
+      "@type": "PostalAddress",
+
+      streetAddress:
+        "Avenida Emilio Fagot 2928 STE 1",
+
+      addressLocality:
+        "Ponce",
+
+      addressRegion:
+        "PR",
+
+      postalCode:
+        "00716",
+
+      addressCountry:
+        "US",
+    },
+
+
+    contactPoint: {
+      "@type": "ContactPoint",
+
+      contactType:
+        "customer service",
+
+      email:
+        "info@cbeipr.com",
+
+      areaServed:
+        "PR",
+    },
+
+
+    hasOfferCatalog: {
+      "@type": "OfferCatalog",
+
+      name:
+        "Servicios psicológicos",
+
+      itemListElement: [
+        {
+          "@type": "Offer",
+
+          itemOffered: {
+            "@type": "Service",
+
+            name:
+              "Psicología Clínica",
+          },
+        },
+
+        {
+          "@type": "Offer",
+
+          itemOffered: {
+            "@type": "Service",
+
+            name:
+              "Terapia Individual y Familiar",
+          },
+        },
+
+        {
+          "@type": "Offer",
+
+          itemOffered: {
+            "@type": "Service",
+
+            name:
+              "Evaluaciones Psicológicas",
+          },
+        },
+      ],
+    },
   };
+
 
   return (
     <script
